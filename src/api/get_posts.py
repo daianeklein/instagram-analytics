bdc6432e8d911bd42f717185617484ce7a916d5b
daianeklein <daiane.klein22@gmail.com>
2025-05-07 18:27:32 -0300

    fix

--- src/api/get_posts.py
+++ src/api/get_posts.py
@@ -3,15 +3,14 @@ import json
 from datetime import datetime
 from dotenv import load_dotenv
 from apify_client import ApifyClient
+from pathlib import Path
 
 class InstagramScraper:
-    def __init__(self, api_key, actor_id, username, result_limit=5, output_dir="data"):
+    def __init__(self, api_key, actor_id, username, result_limit=5):
         self.client = ApifyClient(api_key)
         self.actor_id = actor_id
         self.username = username
         self.result_limit = result_limit
-        self.output_dir = output_dir
-        os.makedirs(self.output_dir, exist_ok=True)
 
     def run_actor(self):
         run_input = {
@@ -24,8 +23,11 @@ class InstagramScraper:
         return list(self.client.dataset(self.run['defaultDatasetId']).iterate_items())
     
     def save_to_json(self, data):
+        current_dir = Path.cwd()
+        output_dir = current_dir.parent / 'data' / 'raw'
+
         date_str = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-        output_path = os.path.join(self.output_dir, f"instagram_ladygaga_{date_str}.json")
+        output_path = os.path.join(output_dir, f"instagram_ladygaga_{date_str}.json")
 
         with open(output_path, "w", encoding="utf-8") as f:
             json.dump(data, f, ensure_ascii=False, indent=2)
@@ -39,8 +41,9 @@ if __name__ == '__main__':
         api_key=os.getenv('API_APIFY'),
         actor_id=os.getenv('ACTOR_ID'),
         username='ladygaga',
-        result_limit=2)
+        result_limit=1)
     
     scraper.run_actor()
     data = scraper.fetch_data()
     scraper.save_to_json(data)
+
