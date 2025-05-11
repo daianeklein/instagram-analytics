9d9a33e88668319d409a5eccd5688be39adc5160
daianeklein <daiane.klein22@gmail.com>
2025-05-11 20:33:41 -0300

    add functional code

--- src/transform/transform_instagram_results.py
+++ src/transform/transform_instagram_results.py
@@ -1,53 +1,37 @@
-# src/transform/instagram_transformer.py
-
 import pandas as pd
-from typing import List, Dict
 from datetime import datetime
+import json
 
-class InstagramPostTransformer:
-    def __init__(self, raw_data: List[Dict]):
-        self.raw_data = raw_data
+def extract_post_data(posts: list) -> pd.DataFrame:
+    records = []
 
-    def transform(self) -> pd.DataFrame:
-        transformed_data = []
+    for post in posts:
+        post_id = post.get("id")
+        caption = post.get("caption", "")
+        likes = post.get("likesCount")
+        timestamp = post.get("timestamp")
 
-        for post in self.raw_data:
-            post_id = post.get("id")
-            caption = post.get("caption")
-            comments_count = post.get("commentsCount")
-            likes_count = post.get("likesCount")
-            timestamp = post.get("timestamp")
+        # Formatação da data e hora
+        if timestamp:
+            dt = datetime.fromisoformat(timestamp.replace("Z", ""))
+            date_str = dt.strftime("%d-%m-%Y")
+            time_str = dt.strftime("%H:%M")
+        else:
+            date_str = None
+            time_str = None
 
-            if timestamp:
-                try:
-                    dt = datetime.fromisoformat(timestamp.replace("Z", ""))
-                    date_str = dt.strftime("%d-%m-%Y")
-                    time_str = dt.strftime("%H:%M:%S")
-                except ValueError:
-                    date_str = None
-                    time_str = None
-            else:
-                date_str = None
-                time_str = None
+        records.append({
+            "post_id": post_id,
+            "caption": caption,
+            "likes_count": likes,
+            "date": date_str,
+            "time": time_str
+        })
+    return pd.DataFrame(records)
 
-            transformed_data.append({
-                "post_id": post_id,
-                "caption": caption,
-                "comments_count": comments_count,
-                "likes_count": likes_count,
-                "date": date_str,
-                "time": time_str
-            })
+path = 'instagram_ladygaga_2025-05-11_20-05-03.json'
+with open(path, "r") as f:
+    raw_data = json.load(f)
 
-        return pd.DataFrame(transformed_data)
-    
-if __name__ == '__main__':
-    import json
-
-    with open('/Users/daianeklein/Documents/DS/instagram-analytics/data/raw/instagram_ladygaga_2025-05-07_19-00-00.json', 'r') as file:
-        data = json.load(file)
-
-        transformer = InstagramPostTransformer(data)
-        df_transformed = transformer.transform()
-        print(df_transformed)
-
+df = extract_post_data(raw_data)
+print(df.head())
