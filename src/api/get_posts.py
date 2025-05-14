d643b5718f5490a0c85fc38a5bd1173fb013b265
daianeklein <daiane.klein22@gmail.com>
2025-05-14 10:39:45 -0300

    add data viz

--- src/api/get_posts.py
+++ src/api/get_posts.py
@@ -35,7 +35,7 @@ def fetch_and_save_posts(instagram_profile: str):
     
     run_input = {
         'username': [instagram_profile],
-        'resultsLimit': 300,  
+        'resultsLimit': 5,  
     }
 
     run = client.actor(actor_id).call(run_input=run_input)
